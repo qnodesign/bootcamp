44a8cd19acaf4bbc5548dea1b56580c7025e7990
qno <hefelle@gmail.com>
2021-03-27 09:16:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello from Max!</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>Gery was here!</h3>
+      <h3>Me too!</h3>
+      <h3>Hey, it's Iryna</h3>
+    </div>
+  );
+}
