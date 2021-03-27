b0f78301c22721f89a2092c7990906691165cb5e
qno <hefelle@gmail.com>
2021-03-27 10:11:39 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import "./styles.css";
+import Login from "./screens/login";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello from Max!</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3>Gery was here!</h3>
-      <h3>Me too!, and me!</h3>
-      <h3>Hey, it's Iryna</h3>
-      <h3>Hi Team - Sergey - here!</h3>
-      <h3>PiotrP did this</h3>
-      <h3>PiotrS</h3>
-      <h1>...</h1>
-      <h3>Henri</h3>
-      <hr />
+      <Login />
     </div>
   );
 }
