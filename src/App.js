ea063af22998d182e3316ff6be7fbf50b50ec907
qno <hefelle@gmail.com>
2021-03-27 09:25:57 +0100

    init

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ export default function App() {
       <h1>Hello from Max!</h1>
       <h2>Start editing to see some magic happen!</h2>
       <h3>Gery was here!</h3>
-      <h3>Me too!</h3>
+      <h3>Me too!, and me!</h3>
       <h3>Hey, it's Iryna</h3>
+      <h3>Hi Team - Sergey - here!</h3>
+      <h3>PiotrP did this</h3>
+      <h3>PiotrS</h3>
+      <h1>...</h1>
+      <h3>Henri</h3>
+      <hr />
     </div>
   );
 }
