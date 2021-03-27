e4c0213e0c027b3213a52de831ac49da142d1d72
qno <hefelle@gmail.com>
2021-03-27 10:47:00 +0100

    dada storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Login from "./screens/login";
+import { useEffect } from "react";
+import { LoginData } from "./data/LoginData";
 import "semantic-ui-css/semantic.min.css";
 
 export default function App() {
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(LoginData));
+  }, []);
+
   return (
     <div className="App">
       <div style={{ maxWidth: "300px", margin: "0 auto", padding: "30px 0" }}>
---
+++ src/data/LoginData.js
@@ -0,0 +1,6 @@
+export const LoginData = [
+  {
+    username: "test",
+    password: "098f6bcd4621d373cade4e832627b4f6"
+  }
+];
