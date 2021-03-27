4e013cb59da42a60c518fae70dc21dfbe1ad8296
qno <hefelle@gmail.com>
2021-03-27 10:08:04 +0100

    Update login.js

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {Form, Button} from 'semantic-ui-react';
+import {Form, Button, Input } from 'semantic-ui-react';
 
 const Login = () => {
   const [form, setForm] = useState({
